b7d2b5a86e592f9ac4294e090d039b0c6f61288d
Superpatate <65410355+PatatePatatePatate@users.noreply.github.com>
2023-06-06 21:25:24 +0200

    Create lvl1-4.c

---
+++ lvl1-4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "robot.h"
+int main()
+{
+haut();
+haut();
+haut();
+droite();
+droite();
+bas();
+bas();
+droite();
+}
+/*
+Écrivez un programme qui affiche exactement le texte qui suit :
+
+↳
+Tout droit tu grimperas,
+La clé tu trouveras,
+Habile tu seras,
+Quand tu les porteras,
+Et avec le chef tu reviendras !
+*/
