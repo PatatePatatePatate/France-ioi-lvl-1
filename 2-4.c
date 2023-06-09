fd32afd1b92e8366d7ae17418590caba3c538c0a
Superpatate <65410355+PatatePatatePatate@users.noreply.github.com>
2023-06-09 22:31:41 +0200

    Update 2-4.c

--- 2-4.c
+++ 2-4.c
@@ -15,7 +15,8 @@ int main()
 L'allée centrale du village peut être représentée comme une série de 17 cases, dont la plupart contiennent une bouse de Borlok :
 
 R...............-
-Le robot se trouve initialement dans la case de gauche. Il doit se déplacer case par case en allant vers la droite, et ramasser sur chaque case la bouse qui s'y trouve. Enfin, votre robot doit déposer tout ce qu'il a ramassé dans la boîte située sur la 17e case, tout à droite.
+Le robot se trouve initialement dans la case de gauche. Il doit se déplacer case par case en allant vers la droite, et ramasser sur chaque case la bouse qui s'y trouve.
+Enfin, votre robot doit déposer tout ce qu'il a ramassé dans la boîte située sur la 17e case, tout à droite.
 
 Votre programme ne doit pas faire plus d'une dizaine de lignes.
 */
