0f6e1ee042daecc293b9b94569c26338b364adb0
Superpatate <65410355+PatatePatatePatate@users.noreply.github.com>
2023-06-10 21:40:17 +0200

    Create 3-05.c

---
+++ 3-05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int longu = 108;
+
+printf("%d\n", longu * longu);
+printf("%d\n", longu * 4);
+}
+/*
+La cour carrée a été mesurée avec quatre bâtons de longueurs respectives 17 m, 7 m, 5 m et 2 m. La longueur du côté de la cour est égale à 5 fois le premier bâton plus 2 fois le second plus 1 fois le troisième plus 2 fois le quatrième.
+
+Votre programme doit afficher deux lignes : la première doit contenir la surface de la cour, et la seconde ligne doit contenir son périmètre. Les résultats doivent être exprimés en mètres carrés et en mètres, respectivement, mais vous ne devez pas afficher l'unité après la valeur numérique.
+
+Important : dans votre programme, commencez par calculer la longueur du côté de la cour et l'enregistrer dans une variable.
+
+
+*/
