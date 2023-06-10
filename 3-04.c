2299c342d15315721f677f613cc3bedf9bd4e7c4
Superpatate <65410355+PatatePatatePatate@users.noreply.github.com>
2023-06-10 21:39:39 +0200

    Create 3-04.c

---
+++ 3-04.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+int algoret = 42;
+
+printf("%d ", algoret);
+printf("%d ", algoret * 2);
+printf("%d\n", algoret * 3);
+
+}
+/*
+L'algoréathlon se constitue de trois étapes à effectuer chaque jour : 2 km de natation, 34 km de cyclisme et 6 km de course à pied.
+
+Sachant qu'un sportif répète ces trois étapes pendant 3 jours de suite, vous devez afficher la distance totale qu'il a parcourue à la fin du 1er jour, à la fin du 2e jour, puis à la fin de l'algoréathlon complet.
+
+Afin de rendre l'affichage convivial sur l'écran du robot, vous souhaitez mettre les trois valeurs sur une même ligne, avec une espace entre chaque valeur et la suivante.
+
+Important : pour écrire ce programme, vous devez mémoriser la distance parcourue en un jour en lui donnant un nom, puis utiliser ce nom pour calculer les trois réponses. Appuyez-vous sur les explications ci-dessous.
+
+
+*/
