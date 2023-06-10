afd536e807e9ded831ad462ef0de95c4695358cc
Superpatate <65410355+PatatePatatePatate@users.noreply.github.com>
2023-06-10 21:36:19 +0200

    Create 3-03.c

---
+++ 3-03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+printf("%d\n", 96 * 3);
+}
+/*
+L'école est formée de 4 classes, constituées respectivement de 25, 30, 27 et 22 élèves. Cependant, 8 élèves sont absents aujourd'hui. Sachant que chaque élève présent doit recevoir 3 bonbons, écrivez un programme qui calcule puis affiche le nombre total de bonbons nécessaires.
+
+Vous n'avez pas besoin de calculatrice : effectuez les calculs dans votre programme.
+*/
