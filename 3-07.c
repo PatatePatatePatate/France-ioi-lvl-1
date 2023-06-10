672ddb903bfd03426ecb61967d7c5b5408b4a1c9
Superpatate <65410355+PatatePatatePatate@users.noreply.github.com>
2023-06-10 21:41:29 +0200

    Create 3-07.c

---
+++ 3-07.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+int main()
+{
+printf("V\n");
+printf("V\n");
+printf("I\n");
+printf("I\n");
+printf("V\n");
+printf("I\n");
+printf("I\n");
+}
+/*
+Vous devez lire attentivement les programmes donnés ci-dessous pour déterminer s'ils sont valides ou non, et ce sans essayer de les exécuter. Pour chacun des 7 programmes, vous devez afficher sur une ligne soit la lettre « V » pour indiquer que le programme est valide, soit la lettre « I » pour indiquer qu'il est invalide. Par exemple, si vous pensez que les 4 premiers programmes s'exécuteront sans erreur et que les 3 suivants entraîneront des erreurs, faites afficher à votre programme :
+
+↳
+V
+V
+V
+V
+I
+I
+I
+Voici les sept programmes.
+
+#include <stdio.h>
+int main()
+{
+   int nbBillesRouges = 4;
+   int nbBillesBleues = 6;
+   printf("%d\n", nbBillesRouges + nbBillesBleues);
+}
+#include <stdio.h>
+int main()
+{
+   int taille = 4;
+   taille = 6;
+   printf("%d\n", taille);
+}
+#include <stdio.h>
+int main()
+{
+   int nbBillesBleues = 3;
+   printf("%d\n", nbBillesRouges);
+}
+#include <stdio.h>
+int main()
+{
+   2 = 2;
+   printf("%d\n", 2);
+}
+#include <stdio.h>
+int main()
+{
+   int age1 = 6;
+   int age2 = 4;
+   age2 = age1;
+   printf("%d\n", age2);
+}
+#include <stdio.h>
+int main()
+{
+   int prix = 10;
+   prix - 2 = prix;
+   printf("%d\n", prix);
+}
+#include <stdio.h>
+int main()
+{
+   int prix = age - 7;
+   int age = 12;
+   printf("%d\n", prix);
+}
+*/
