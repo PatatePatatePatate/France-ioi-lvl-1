01119ed32a954475c434e22c4b0c864dcc908180
Superpatate <65410355+PatatePatatePatate@users.noreply.github.com>
2023-06-10 21:35:49 +0200

    Create 3-02.c

---
+++ 3-02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+printf("%d\n", 12581 - 11937);
+}
+/*
+Vous savez que l'éclipse aura lieu le 12581e jour et que la date actuelle est le 11937e jour. Votre programme doit calculer et afficher le nombre de jours qu'il faudra patienter avant de pouvoir admirer l'éclipse.
+*/
